385bbdc0e1b0bd0d184e6301607eeb512801982d
dangkhoaicao <dangkhoaicao@gmail.com>
2018-09-20 19:09:35 +0700

    intergrate uikit to angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var UIkit: any;
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'my-angular-uikit';
+
+  ngOnInit() {
+  }
+
+  showAlert(): void {
+    UIkit.modal.alert('This is UI KIT Alert!');
+  }
+
 }
